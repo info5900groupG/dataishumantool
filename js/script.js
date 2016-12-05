91d651c75d533084086467d84cb835bef8267c74
thevaleriemack <dgm97@cornell.edu>
2016-12-05 00:09:41 -0500

    sub sections for data analysis tab

--- js/script.js
+++ js/script.js
@@ -1,14 +1,22 @@
-$(function() {
-  
-  $("#timeline-view").click(function() {
-    window.location.href = "../pages/timeline.html"
-  });
+//$(function() {
+//  
+//  $("#timeline-view").click(function() {
+//    window.location.href = "../pages/timeline.html"
+//  });
+//
+//});
 
+//Dropdown
+//$('[gg-data-toggle="dropdown"]').dropdown();
+
+$('#gg-caret').click(function() {
+    $('#gg-span-dropdown').dropdown("toggle");
+    $('#gg-ul-dropdown').toggle();
 });
 
 // Back to top
 $(window).scroll(function() {
-    if ($(this).scrollTop() >= 100) {
+    if ($(this).scrollTop() > 340) {
         $('#gg-to-top').fadeIn(200);
     } else {
         $('#gg-to-top').fadeOut(200);
@@ -18,4 +26,22 @@ $('#gg-to-top').click(function() {
     $('body,html').animate({
         scrollTop : 340
     }, 500);
+});
+
+
+// Smooth jump
+$(function() {
+  $('a[class="smoothJump"]').click(function() {
+      console.log('smoothJump ed');
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top-140
+        }, 1000);
+        return false;
+      }
+    }
+  });
 });
