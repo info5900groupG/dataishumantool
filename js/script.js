3f06547654d0ba96655b83a22691bb1fcb0e7d7a
thevaleriemack <dgm97@cornell.edu>
2016-12-04 21:16:15 -0500

    back to top button

--- js/script.js
+++ js/script.js
@@ -5,3 +5,17 @@ $(function() {
   });
 
 });
+
+// Back to top
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 100) {
+        $('#gg-to-top').fadeIn(200);
+    } else {
+        $('#gg-to-top').fadeOut(200);
+    }
+});
+$('#gg-to-top').click(function() {
+    $('body,html').animate({
+        scrollTop : 340
+    }, 500);
+});
