02bfa1946cd0d1952aa65f9f220f74f4d3fb167f
thevaleriemack <dgm97@cornell.edu>
2016-12-08 18:31:03 -0500

    js css fixes

--- js/script.js
+++ js/script.js
@@ -8,12 +8,16 @@
 
 // Learn more
 $('#gg-learn-more').click(function() {
+    if ($(this).find('i').hasClass('fa-search-plus')) {
+        $(this).find('i').attr('class', 'fa fa-search-minus');
+    } else {
+        $(this).find('i').attr('class', 'fa fa-search-plus');
+    }
     $('#gg-bg-well').slideToggle();
 });
 
 //Dropdown
 //$('[gg-data-toggle="dropdown"]').dropdown();
-
 $('#gg-caret').click(function() {
     $('#gg-span-dropdown').dropdown("toggle");
     $('#gg-ul-dropdown').toggle();
@@ -21,7 +25,11 @@ $('#gg-caret').click(function() {
 
 // Back to top
 $(window).scroll(function() {
-    if ($(this).scrollTop() > 340) {
+    if ($(this).scrollTop() > 100) {
+        $('#gg-bg-well').slideUp();
+        $('#gg-learn-more').find('i').attr('class', 'fa fa-search-plus');
+    }
+    if ($(this).scrollTop() > 480) {
         $('#gg-to-top').fadeIn(200);
     } else {
         $('#gg-to-top').fadeOut(200);
@@ -29,7 +37,7 @@ $(window).scroll(function() {
 });
 $('#gg-to-top').click(function() {
     $('body,html').animate({
-        scrollTop : 440
+        scrollTop : 480
     }, 500);
 });
 
